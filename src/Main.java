65ddfdfc3d1688bac891f439f3e1b85cff695f8f
Maikl-ge <503414@gmail.com>
2022-11-28 23:18:38 +0400

    First ver

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        {
+mport java.io.*;
+import java.util.Scanner;
 
-public static void
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Interface form = new Interface();
+        form.setVisible(true);
+
+
+    }
+
+
+
+
+
+
+
+//            FileWriter nFile = new FileWriter("file1.txt");
+//
+//            nFile.write("Хокку родом из глубин России \nПодобен лучу самурайский клинок \nИ тот затупился \nПроклятая килька в томате!!");
+//
+//            nFile.close();
+//        FileReader fr= new FileReader("file2.txt");
+//        Scanner scan = new Scanner(fr);
+//
+//
+//        int i = 1;
+//
+//        while (scan.hasNextLine()) {
+//            System.out.println(i + " : " + scan.nextLine());
+//            i++;
+//        }
+//
+//        fr.close();
+//        }
 }
